f3c5f6b09a84783528f271bc086401e5b33f0663
shouemura <uemura@quail.co.jp>
2023-11-26 22:10:25 +0900

    feat: meta情報をjsonで管理してページ別にincludeする仕組みを追加（EJS）

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,43 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const fileInclude = require("gulp-file-include");
+const rename = require("gulp-rename");
+const ejs = require("gulp-ejs");
+const fs = require("fs");
 
-//デフォルトタスク
-gulp.task("default", function () {
-  //sassのコンパイル
+
+// Sassコンパイル
+const SassCompile = (done) => {
   gulp
     .src("src/assets/scss/*.scss")
     .pipe(sass({ outputStyle: "expanded" }))
     .pipe(gulp.dest("./server/assets/css"));
-});
+};
+exports.SassCompile = SassCompile;
+
+// EJSコンパイル
+const ejsCompile = () => {
+  var json = JSON.parse(fs.readFileSync("./src/assets/json/meta.json", "utf-8"));
+  gulp
+    .src(["./src/**/*.ejs", "!" + "./src/**/_*.ejs"])
+    .pipe(ejs({ json: json }))
+    .pipe(
+      rename({
+        extname: ".html",
+      })
+    )
+    .pipe(gulp.dest("./server/"));
+};
+exports.ejsCompile = ejsCompile;
+
+// 監視ファイル
+const watchFiles = (done) => {
+  gulp.watch(["./src/**/*.ejs", "!./src/**/_*.ejs"], ejsCompile);
+  done();
+};
+
+// タスク実行
+exports.default = gulp.series(watchFiles, ejsCompile);
 
 gulp.task("include", function () {
   //includeのコンパイル
